ab0b50e55b3011db12a21d2faeb52addf63c05ad
Kamille Richards <krichards44@gatech.edu>
2026-06-18 11:00:03 -0700

    Add try{} throughout code
    Ensure "try{} catch(error) {}" is added throughout the code
    in the appropriate places to make sure all errors are caught,
    even those that were not anticipated.

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/register', async (req, res, next) => {
         const token = jwt.sign({ id: newUser.id, email: newUser.email }, JWT_SECRET, { expiresIn: "15m" });
         return res.json({ token });
     } catch (error) {
-        console.error(error);
+        // console.error(error);
         next(error);
     }
 });
@@ -90,90 +90,104 @@ app.post('/login', (req, res, next) => {
 });
 
 // Create a to-do item
-app.post('/todos', passport.authenticate("jwt", { session: false }), async (req, res) => {
+app.post('/todos', passport.authenticate("jwt", { session: false }), async (req, res, next) => {
     // if authentication is unsuccessful, the response is 401 Unauthorized
 
     // Check that there's a request body and that it contains title and description
-    if(!req.body || !req.body.title || !req.body.description) {
+    if (!req.body || !req.body.title || !req.body.description) {
         return res.status(400).send('Title and Description are required.');
     }
 
-    // Add task to database table
-    await db.insertTask(req.body.title, req.body.description, req.user.id);
+    try {
+        // Add task to database table
+        await db.insertTask(req.body.title, req.body.description, req.user.id);
 
-    // Get task from database table, so you can get the id from the database table
-    const task = await db.getLatestTask(req.body.title, req.body.description, req.user.id);
+        // Get task from database table, so you can get the id from the database table
+        const task = await db.getLatestTask(req.body.title, req.body.description, req.user.id);
 
-    // Upon successful creation of the to-do item, respond with the details of the created item.
-    return res.json({
-        id: task.id,
-        title: task.title,
-        description: task.description
-    });
-
+        // Upon successful creation of the to-do item, respond with the details of the created item.
+        return res.json({
+            id: task.id,
+            title: task.title,
+            description: task.description
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // Update a to-do item
-app.put('/todos/:id', passport.authenticate("jwt", { session: false }), async (req, res) => {
+app.put('/todos/:id', passport.authenticate("jwt", { session: false }), async (req, res, next) => {
     // if authentication is unsuccessful, the response is 401 Unauthorized
 
     // Check that request body contains title and description
-    if(!req.body || !req.body.title || !req.body.description) {
+    if (!req.body || !req.body.title || !req.body.description) {
         return res.status(400).send('Title and Description are required.');
     }
 
-    // Update database table record
-    const id = parseInt(req.params.id, 10);
-    await db.updateTask(req.body.title, req.body.description, id);
+    try {
+        // Update database table record
+        const id = parseInt(req.params.id, 10);
+        await db.updateTask(req.body.title, req.body.description, id);
 
-    // Get record that was just updated from the database table
-    const task = await db.getTask(id);
+        // Get record that was just updated from the database table
+        const task = await db.getTask(id);
 
-    // Upon successful update of the to-do item, respond with the details of the updated item.
-    return res.json({
-        id: task.id,
-        title: task.title,
-        description: task.description
-    });
-
+        // Upon successful update of the to-do item, respond with the details of the updated item.
+        return res.json({
+            id: task.id,
+            title: task.title,
+            description: task.description
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // Delete a to-do item
-app.delete('/todos/:id', passport.authenticate("jwt", { session: false }), async (req, res) => {
+app.delete('/todos/:id', passport.authenticate("jwt", { session: false }), async (req, res, next) => {
     // if authentication is unsuccessful, the response is 401 Unauthorized
 
-    // Delete task from database table
-    const id = parseInt(req.params.id, 10);
-    await db.deleteTask(id);
+    try {
+        // Delete task from database table
+        const id = parseInt(req.params.id, 10);
+        await db.deleteTask(id);
 
-    // Upon successful deletion of the to-do item, respond with a 204 status code
-    return res.status(204).send();
+        // Upon successful deletion of the to-do item, respond with a 204 status code
+        return res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
 });
 
 // Get to-do items
-app.get('/todos', passport.authenticate("jwt", { session: false }), async (req, res) => {
+app.get('/todos', passport.authenticate("jwt", { session: false }), async (req, res, next) => {
     // if authentication is unsuccessful, the response is 401 Unauthorized
 
     // Check that request query contains page and limit
-    if(!req.query || !req.query.page || !req.query.limit) {
+    if (!req.query || !req.query.page || !req.query.limit) {
         return res.status(400).send('URL should look like: http://localhost:3000/todos?page=1&limit=10 with page and limit.');
     }
 
-    // Convert info from req query from string to numbers, e.g. if path is entered as http://localhost:3000/todos?page=1&limit=10
-    const page = parseInt(req.query.page, 10);
-    const limit = parseInt(req.query.limit, 10);
+    try {
+        // Convert info from req query from string to numbers, e.g. if path is entered as http://localhost:3000/todos?page=1&limit=10
+        const page = parseInt(req.query.page, 10);
+        const limit = parseInt(req.query.limit, 10);
 
-    // Get tasks and count of tasks to return from database table
-    const tasks = await db.getAllTasks();
-    const count = await db.getTaskCount();
+        // Get tasks and count of tasks to return from database table
+        const tasks = await db.getAllTasks();
+        const count = await db.getTaskCount();
 
-    // Respond with the list of to-do items along with the pagination details.
-    return res.json({
-        data: tasks,
-        page: page,
-        limit: limit,
-        total: count
-    });
+        // Respond with the list of to-do items along with the pagination details.
+        return res.json({
+            data: tasks,
+            page: page,
+            limit: limit,
+            total: count
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
 const PORT = 3000;
